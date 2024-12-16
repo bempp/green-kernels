e50bc781f6f3c0a7935f14fb647fee21cbcf6bca
Timo Betcke <timo.betcke@gmail.com>
2024-12-16 13:18:46 +0000

    Removed MPI as default dependenc

--- src/traits.rs
+++ src/traits.rs
@@ -1,6 +1,11 @@
 //! Trait for Green's function kernels
+
 use crate::types::GreenKernelEvalType;
+#[cfg(feature = "mpi")]
+use mpi::traits::{Communicator, Equivalence, Root};
 use rlst::RlstScalar;
+#[cfg(feature = "mpi")]
+use rlst::{rlst_dynamic_array1, DistributedVector, IndexLayout, RawAccess, RawAccessMut};
 
 /// Interface to evaluating Green's functions for given sources and targets.
 pub trait Kernel: Sync {
@@ -109,3 +114,88 @@ pub trait Kernel: Sync {
     /// given, and `4` if [EvalType::ValueDeriv] is given.
     fn range_component_count(&self, eval_type: GreenKernelEvalType) -> usize;
 }
+
+// Note that we cannot just add the `evaluate_distributed` method to the `Kernel` trait
+// since currently the C interface is implemented by making `Kernel` a trait object.
+// This requires that methods do not introduce additional template parameters. Can change this
+// again once we move to the better C interface in `c-api-tools`.
+
+/// Distributed evaluation of a Green's function kernel.
+#[cfg(feature = "mpi")]
+pub trait DistributedKernelEvaluator: Kernel {
+    fn evaluate_distributed<
+        SourceLayout: IndexLayout,
+        TargetLayout: IndexLayout,
+        ChargeLayout: IndexLayout,
+        ResultLayout: IndexLayout,
+    >(
+        &self,
+        eval_type: GreenKernelEvalType,
+        sources: &DistributedVector<'_, SourceLayout, <Self::T as RlstScalar>::Real>,
+        targets: &DistributedVector<'_, TargetLayout, <Self::T as RlstScalar>::Real>,
+        charges: &DistributedVector<'_, ChargeLayout, Self::T>,
+        result: &mut DistributedVector<'_, ResultLayout, Self::T>,
+    ) where
+        Self::T: Equivalence,
+        <Self::T as RlstScalar>::Real: Equivalence,
+    {
+        // We want that everything has the same communicator
+        assert!(std::ptr::addr_eq(
+            sources.index_layout().comm(),
+            charges.index_layout().comm()
+        ));
+        assert!(std::ptr::addr_eq(
+            sources.index_layout().comm(),
+            targets.index_layout().comm()
+        ));
+        assert!(std::ptr::addr_eq(
+            sources.index_layout().comm(),
+            result.index_layout().comm()
+        ));
+
+        // Check that the output vector has the correct size.
+        assert_eq!(
+            targets.index_layout().number_of_local_indices(),
+            3 * result.index_layout().number_of_local_indices()
+        );
+
+        let size = sources.index_layout().comm().size();
+
+        // We now iterate through each rank associated with the sources and communicate from that rank
+        // the sources to all target ranks.
+
+        for rank in 0..size as usize {
+            // Communicate the sources and charges from `rank` to all ranks.
+
+            let root_process = sources.index_layout().comm().process_at_rank(rank as i32);
+            let source_range = sources.index_layout().index_range(rank).unwrap();
+            let charge_range = charges.index_layout().index_range(rank).unwrap();
+            let nsources = source_range.1 - source_range.0;
+            let ncharges = charge_range.1 - charge_range.0;
+            // Make sure that number of sources and charges are compatible.
+            assert_eq!(nsources, 3 * ncharges);
+            let mut root_sources = rlst_dynamic_array1!(<Self::T as RlstScalar>::Real, [nsources]);
+            let mut root_charges = rlst_dynamic_array1!(Self::T, [ncharges]);
+            // If we are on `rank` fill the sources and charges.
+            if sources.index_layout().comm().rank() == rank as i32 {
+                root_sources.fill_from(sources.local().r());
+                root_charges.fill_from(charges.local().r());
+            }
+
+            root_process.broadcast_into(&mut root_sources.data_mut()[..]);
+            root_process.broadcast_into(&mut root_charges.data_mut()[..]);
+
+            // We now have the sources and charges on all ranks. We can now simply evaluate.
+            self.evaluate_mt(
+                eval_type,
+                &root_sources.data()[..],
+                targets.local().data(),
+                &root_charges.data()[..],
+                result.local_mut().data_mut(),
+            );
+        }
+    }
+}
+
+#[cfg(feature = "mpi")]
+impl<K: Kernel> DistributedKernelEvaluator for K {}
